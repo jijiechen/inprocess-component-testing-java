a0fbec540cef5cbd0e0a6411c65f9274e27dcbf8
Jijie Chen <jijie.chen@someplace.com>
2017-11-23 00:56:22 +0800

    Added a few more test cases for the sample Spring controller

--- src/main/java/org/twj/component/testing/HelloWorldController.java
+++ src/main/java/org/twj/component/testing/HelloWorldController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Map;
-
 @Controller
 public class HelloWorldController {
 
@@ -14,7 +12,7 @@ public class HelloWorldController {
     private HelloWorldService helloWorldService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Map<String, Object> model) {
+    public String index() {
         return helloWorldService.sayHello();
     }
 }
--- src/test/java/org/twj/component/testing/HelloWorldControllerTest.java
+++ src/test/java/org/twj/component/testing/HelloWorldControllerTest.java
@@ -1,14 +1,20 @@
 package org.twj.component.testing;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-app.xml"})
@@ -21,11 +27,41 @@ public class HelloWorldControllerTest {
     }
 
     @Autowired
-    private HelloWorldController controller;
+    private HelloWorldController helloController;
+    @Autowired
+    private HelloWorldService helloService;
+    @Autowired
+    ApplicationContext appContext;
 
 
     @Test
     public void controller_should_not_be_null(){
-        assertThat(controller).isNotNull();
+        assertThat(helloController).isNotNull();
+    }
+
+    @Test
+    public void controller_should_work_as_expected(){
+        // HelloWorldController ctrl = appContext.getBean(HelloWorldController.class);
+
+        String helloResult = helloController.index();
+        assertEquals("Hello Spring Application" ,helloResult);
+    }
+
+    @Test
+    public void controller_service_should_be_able_to_be_mocked(){
+        HelloWorldService serviceMock = mock(HelloWorldService.class);
+        when(serviceMock.sayHello()).thenReturn("Mocked Hello World");
+
+        setField(helloController, "helloWorldService", serviceMock);
+        String helloResult = helloController.index();
+
+
+        assertEquals("Mocked Hello World" ,helloResult);
+    }
+
+
+    @After
+    public void restore(){
+        setField(helloController, "helloWorldService", helloService);
     }
 }
