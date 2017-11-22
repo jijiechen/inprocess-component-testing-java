b7ad49ab17365146500ebf5014d64643b4856a69
Jijie Chen <jijie.chen@someplace.com>
2017-11-22 23:14:19 +0800

    Initial commit: initialized an empty Java project.

---
+++ src/test/java/org/twj/component/testing/HelloWorldControllerTest.java
@@ -0,0 +1,13 @@
+package org.twj.component.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldControllerTest {
+
+    @Test
+    public void should_pass(){
+        assertTrue(true);
+    }
+}
